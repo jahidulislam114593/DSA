d7c33e4203622fc307d670361df883602b72dda4
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-21 12:54:09 +0600

    [optimized using two-pointer approch]

--- pairSum.cpp
+++ pairSum.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int>pairSum(vector<int> arr, int k)
+vector<int>pairSumBruteForce(vector<int> arr, int k)
 {
+	// return pair in sorted array with target sum
+	//BruteForce TC--> O(n^2)
+
 	int n = arr.size();
 	vector<int> ans;
 	for(int i = 0; i<n; i++)
@@ -11,11 +14,38 @@ vector<int>pairSum(vector<int> arr, int k)
 		{
 			if(arr[i] + arr[j] == k)
 			{
-				ans.push_back(arr[i]);
-				ans.push_back(arr[j]);
+				ans.push_back(i);
+				ans.push_back(j);
 				return ans;
 			}
 		}
+	}
+	return ans;
+
+
+}
+
+vector<int>pariSumTwoPointer(vector<int>arr, int k)
+{
+	// Two Pointer Approch (Sorted) TC--> O(n)
+
+	vector<int>ans;
+	int n = arr.size();
+	int i = 0;
+	int j = n - 1;
+	while(i != j)
+	{
+		int ps = arr[i] + arr[j];
+		if(ps > k) j --;
+		else if(ps < k) i++;
+		else 
+		{
+			ans.push_back(i);
+			ans.push_back(j);
+			return ans;
+		}
+
+
 	}
 	return ans;
 }
@@ -23,9 +53,11 @@ vector<int>pairSum(vector<int> arr, int k)
 int main()
 {
 	vector<int> nums ={2, 7, 11, 15};
-	int target = 22; 
+	int target = 13; 
 	
-	vector<int>sum = pairSum(nums, target);
-	cout<<sum[0]<<", "<<sum[1]<<endl;
-	
+	vector<int>psBruteForce = pairSumBruteForce(nums, target);
+	cout<<psBruteForce[0]<<", "<<psBruteForce[1]<<endl;
+
+	vector<int>psTwopointer = pariSumTwoPointer(nums, target);
+	cout<<psTwopointer[0]<<", "<<psTwopointer[1]<<endl;
 }
