6fcd848ea4de3333622243fdd3df0f48554864bf
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-26 12:07:42 +0600

    [Binary Exponentiation for positive TC-> O(logn)]

--- binaryExpo.cpp
+++ binaryExpo.cpp
@@ -3,9 +3,26 @@ using namespace std;
 
 int main()
 {
+	//n^x
+	// int n; cin>>n;
+	// int x; cin>>x;
+	// int ans = 1;
+	// for(int i = 1; i <= x; i++) ans *= n ;
+	// cout<<ans<<endl;
+
 	int n; cin>>n;
 	int x; cin>>x;
-	int ans = 1;
-	for(int i = 1; i <= x; i++) ans *= n ;
+	int binaryForm = x;
+	int ans = 1; 
+
+	while(binaryForm > 0)
+	{
+		if(binaryForm % 2 == 1)
+		{
+			ans *= n;
+		}
+		n *= n;
+		binaryForm /= 2;
+	}
 	cout<<ans<<endl;
 }
