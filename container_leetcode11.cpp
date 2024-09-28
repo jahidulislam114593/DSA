3bea042d6fdf842f774db8bba4c3fe526598b28e
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-28 16:16:54 +0600

    [Two Pointer Approch for container with most water]

--- container_leetcode11.cpp
+++ container_leetcode11.cpp
@@ -4,22 +4,40 @@ using namespace std;
 int main()
 {
 	//BruteForce 
-	vector<int> arr ={1, 8, 6, 2, 5, 4, 8, 3, 7};
-	int n = arr.size();
-	int mx_area = 0;
+	// vector<int> arr ={1, 8, 6, 2, 5, 4, 8, 3, 7};
+	// int n = arr.size();
+	// int mx_area = 0;
 
-	for(int left = 0; left<n; left++)
-	{
-		int area = 0;
-		for(int right = left+1; right<n; right++)
-		{
-			int width = right - left;
-			int height = min(arr[right], arr[left]);
-			area = height * width;
-			mx_area = max(mx_area, area);
-		}
+	// for(int left = 0; left<n; left++)
+	// {
+	// 	int area = 0;
+	// 	for(int right = left+1; right<n; right++)
+	// 	{
+	// 		int width = right - left;
+	// 		int height = min(arr[right], arr[left]);
+	// 		area = height * width;
+	// 		mx_area = max(mx_area, area);
+	// 	}
 		
 
+	// }
+	// cout<<mx_area<<endl;
+
+
+	//Two Pointer Approch ->O(n)
+	vector<int> arr ={1, 8, 6, 2, 5, 4, 8, 3, 7};
+	int n = arr.size();
+
+	int lp = 0, rp = n-1;
+	int maxWater = 0;
+	while(lp < rp)
+	{
+		int w = rp - lp;
+		int ht = min(arr[lp], arr[rp]);
+		int currrentWater = w * ht;
+		maxWater = max(maxWater, currrentWater);
+
+		arr[lp] < arr[rp] ? lp++ : rp--;
 	}
-	cout<<mx_area<<endl;
+	cout<<maxWater<<endl;
 }
