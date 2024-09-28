02759510f44a832fd75b2ce551e6783d11b2db43
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-28 15:40:11 +0600

    [BruteForce of leetcode 11. Container With Most Water]

---
+++ container_leetcode11.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	//BruteForce 
+	vector<int> arr ={1, 8, 6, 2, 5, 4, 8, 3, 7};
+	int n = arr.size();
+	int mx_area = 0;
+
+	for(int left = 0; left<n; left++)
+	{
+		int area = 0;
+		for(int right = left+1; right<n; right++)
+		{
+			int width = right - left;
+			int x = min(arr[right], arr[left]);
+			area = x * width;
+			mx_area = max(mx_area, area);
+		}
+		
+
+	}
+	cout<<mx_area<<endl;
+}
