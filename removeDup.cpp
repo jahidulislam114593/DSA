6563dabbf3d4a46ff5c41169199084fc0875618d
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-30 13:41:03 +0600

    [Two pointer approch ]

--- removeDup.cpp
+++ removeDup.cpp
@@ -3,29 +3,53 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    // vector<int> arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    // int n = arr.size();
+    // set<int> st;
+
+    // // Insert unique elements into the set
+    // for (int i = 0; i < n; i++)
+    // {
+    //     st.insert(arr[i]);
+    // }
+
+    // // Now overwrite the array using the set (which only contains unique elements)
+    // int idx = 0;
+    // for (auto i: st)
+    // {
+    //     arr[idx] = i;
+    //     idx++;
+    // }
+
+    // cout<<idx<<endl;
+    // // Print only the unique elements
+    // for (int i = 0; i < idx; i++) 
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+
+
+
+	//two pointer approch 
+	vector<int> arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
     int n = arr.size();
-    set<int> st;
 
-    // Insert unique elements into the set
-    for (int i = 0; i < n; i++)
+    int i = 1;
+    for(int j = 1; j < n; j++)
     {
-        st.insert(arr[i]);
+    	if(arr[j] != arr[i-1])
+    	{
+    		arr[i] = arr[j];
+    		i++;
+    	}
     }
 
-    // Now overwrite the array using the set (which only contains unique elements)
-    int idx = 0;
-    for (auto i: st)
-    {
-        arr[idx] = i;
-        idx++;
-    }
+    for(int i: arr) cout<<i<<" ";
+    	cout<<endl;
 
-    // Print only the unique elements
-    for (int i = 0; i < idx; i++) 
-    {
-        cout << arr[i] << " ";
-    }
+    cout<<i<<endl; // return i this is the answer
+
 
     return 0;
 }
