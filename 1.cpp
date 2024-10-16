f01a9f2a8c81dc96ec68e2633c5704de95b304a0
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-16 11:18:50 +0600

    [1539. Kth Missing Positive Number]

--- 1.cpp
+++ 1.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main()
 {
-    vector<int>arr = {1, 2, 3, 4};
-    int n = arr.size();
-    int k = 2;
-    
+    if(3<3)cout<<"true";
+     
 
 
 }
--- kthMissing.cpp
+++ kthMissing.cpp
@@ -3,28 +3,38 @@ using namespace std;
 
 int main()
 {
+	//Brute Force 
+	// vector<int> arr = {1, 2, 3, 4} ;
+	// int n = arr.size();
+	// int k = 2;
+
+	// for(int i = 0; i<n; i++)
+	// {
+	// 	if(arr[i] <= k) k++;
+	// 	else break;
+	// }
+	// cout<<k<<endl;
+
+
+	//Binary Search
+	// formula for missing element = arr[end] + more
+	//more-> k - missing
+	//missing -> arr[end] - (end+1)
+	//ans = k + end + 1 or st + 1;
+
 	vector<int> arr = {1, 2, 3, 4} ;
 	int n = arr.size();
 	int k = 2;
 
-	int current = 1;
-	int missing = 0;
-	int idx = 0;
-	while(true)
+	int st = 0, end = n - 1;
+	while(st <= end)
 	{
-		if(idx < arr.size() && arr[idx] == current)
-		{
-			idx++;
-		}else
-		{
-			missing++;
-			if(missing == k) 
-			{
-				break;
-			}
-		}
-		current++;
+		int mid = st + (end - st) / 2;
+		int missing = arr[mid] - (mid + 1);
+		if(missing < k) st = mid + 1;
+		else end = mid - 1;
 	}
-	cout<<current<<endl;
+	// cout<<st+k<<endl;
+	cout<<end + k + 1;
 
 }
