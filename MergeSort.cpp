abf788c6ebd6cbc600d98aa489ccae9fe55d0061
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-24 11:50:43 +0600

    [feat: Add multiple approaches to merge sorted arrays (LeetCode 88)]

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    vector<int> num1 = {1, 2, 3, 0, 0, 0};  // nums1 array
-    vector<int> num2 = {2, 5, 6};           // nums2 array
+    vector<int> num1 = {4, 5, 6, 0, 0, 0};  // nums1 array
+    vector<int> num2 = {1, 2, 3};           // nums2 array
     int m = 3, n = 3;  // m = number of valid elements in num1, n = size of num2
 
     // Brute Force Approach: Merging two arrays with extra space
@@ -24,13 +24,32 @@ int main() {
     */
 
     //Approach: Merge in-place by appending and sorting
-    for(int i = 0; i < n; ++i) {
-        num1[m + i] = num2[i];  // Copy elements from num2 to the back of num1
-    }
-    sort(num1.begin(), num1.end());  // Sort the combined array
+    // for(int i = 0; i < n; ++i) {
+    //     num1[m + i] = num2[i];  // Copy elements from num2 to the back of num1
+    // }
+    // sort(num1.begin(), num1.end());  // Sort the combined array
 
-    // Output sorted merged array
+    // // Output sorted merged array
+    // for(int x : num1) cout << x << " ";
+
+
+    int idx = m+n-1;
+    int i = m - 1;
+    int j = n - 1;
+
+    while(j>=0)
+    {
+    	if(i>=0 && num1[i] > num2[j])
+    	{
+    		num1[idx] = num1[i--];
+    	}else
+    	{
+    		num1[idx] = num2[j--];
+    	}
+    	idx--;
+    }
     for(int x : num1) cout << x << " ";
+
 
     return 0;
 }
